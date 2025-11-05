1e99f1eb36e00ba43be9f4fd6c06a1a146ffb2b2
duvan-virtualsoft <holger.esparza@virtualsoft.tech>
2025-11-04 22:35:24 -0500

    agregacion de test

--- SimpleMavenProject/src/test/java/edu/unac/CalculatorTest.java
+++ SimpleMavenProject/src/test/java/edu/unac/CalculatorTest.java
@@ -1,13 +1,37 @@
 package edu.unac;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
-    @Test
-    void testAdd(){
+    private final Calculator calculator = new Calculator();
 
+    @Test
+    void testAdd() {
+        assertEquals(5, calculator.add(2, 3));
+        assertEquals(0, calculator.add(-1, 1));
+        assertEquals(-5, calculator.add(-2, -3));
+    }
+
+    @Test
+    void testMultiply() {
+        assertEquals(6, calculator.multiply(2, 3));
+        assertEquals(0, calculator.multiply(0, 5));
+        assertEquals(-10, calculator.multiply(-2, 5));
+    }
+
+    @Test
+    void testSubtract() {
+        assertEquals(1, calculator.subtract(3, 2));
+        assertEquals(-2, calculator.subtract(3, 5));
+        assertEquals(0, calculator.subtract(5, 5));
+    }
+
+    @Test
+    void testDivision() {
+        assertEquals(2, calculator.division(6, 3));
+        assertEquals(0, calculator.division(0, 5));
+        assertThrows(ArithmeticException.class, () -> calculator.division(5, 0));
     }
 }
